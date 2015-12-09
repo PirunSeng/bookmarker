ba51bdf1972363f2139f664312373dd82ebf91a4
Pirun SENG <sengpirun.rain@gmail.com>
2015-12-09 11:03:13 +0700

    validate category and use font nimbus-sans

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -2,6 +2,8 @@ class Finder < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
 
+  validates :category, presence: true
+
   scope :rubyonrails, -> { where(category_id: 1).order(:name) }
   scope :jquery, -> { where(category_id: 2).order(:name) }
 end
