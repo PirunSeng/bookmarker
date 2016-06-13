d51a68d992b054bb79e65c8e0cb9ce1cc47abfa9
Pirun SENG <sengpirun.rain@gmail.com>
2016-06-13 13:28:23 +0700

    Remove Category

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -1,13 +1,7 @@
 class Finder < ActiveRecord::Base
-  belongs_to :category
   belongs_to :user
 
-  validates :category,  presence: true
-  validates :name,      presence: true, uniqueness: true
-  validates :reference, presence: true, uniqueness: true
-
-  scope :rubyonrails, -> { where(category_id: 1).order(:name) }
-  scope :jquery, -> { where(category_id: 2).order(:name) }
+  validates :name, :reference, presence: true, uniqueness: { case_sensitive: false }
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :categories
   has_many :finders
 
   def admin?
