6ccf6fa3a2729889214c6212f207e45513c3e9ce
Pirun SENG <sengpirun.rain@gmail.com>
2016-09-29 13:47:37 +0700

    Links and validates URI format

--- app/grids/finder_grid.rb
+++ app/grids/finder_grid.rb
@@ -5,11 +5,14 @@ class FinderGrid
     Finder.all
   end
 
-  filter(:name, :string) { |value, scope| scope.name_like(value) }
+  filter(:name, :string, header: 'Title') { |value, scope| scope.name_like(value) }
   filter(:reference, :string) { |value, scope| scope.reference_like(value) }
   filter(:description, :string) { |value, scope| scope.description_like(value) }
 
-  column(:name)
-  column(:reference)
+  column(:name, header: 'Title')
+  column(:reference, html: true) do |object|
+    link_to object.reference, object.reference, target: :_blank
+  end
+  column(:reference, html: false)
   column(:description)
 end
--- app/models/finder.rb
+++ app/models/finder.rb
@@ -1,7 +1,11 @@
 class Finder < ActiveRecord::Base
+  require 'uri'
   belongs_to :user
 
   validates :name, :reference, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :reference, with: /\A(ftp|http|https)\:\/\/[a-z]+(\W)*([a-z])*[.]([a-z]+)(.*)[a-z]\z/, message: 'should match "https://www.myexample.com"'
+
+  default_scope { order(created_at: :desc) }
 
   scope :name_like,        -> (value) { where('LOWER(finders.name) LIKE ?', "%#{value.downcase}%") }
   scope :reference_like,   -> (value) { where('LOWER(finders.reference) LIKE ?', "%#{value.downcase}%") }
