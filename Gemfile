f58ec8ee720470eaea769e19557ef985a1697081
PirunSeng <sengpirun.rain@gmail.com>
2017-02-03 22:35:49 +0700

    Resetup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-ruby '2.2.0'
-
 gem 'cancancan',                '~> 1.13', '>= 1.13.1'
 gem 'coffee-rails',             '~> 4.1.0'
 gem 'datagrid',                 '~> 1.4', '>= 1.4.4'
@@ -22,6 +20,7 @@ gem 'simple_form',              '~> 3.2'
 gem 'uglifier',                 '>= 1.3.0'
 gem 'will_paginate',            '~> 3.0', '>= 3.0.7'
 gem 'will_paginate-materialize'
+gem 'dotenv-rails',             '~> 2.1', '>= 2.1.1'
 # gem 'draper',                 '~> 2.1'
 # gem 'select2-rails',          '~> 3.5.9.3'
 # gem 'whenever',               '~> 0.9.4'
@@ -29,7 +28,6 @@ gem 'will_paginate-materialize'
 
 group :development, :test do
   gem 'byebug'
-  gem 'sqlite3', '~> 1.3', '>= 1.3.11'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160613183302) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20160613183302) do
     t.integer  "user_id"
   end
 
-  add_index "categories", ["user_id"], name: "index_categories_on_user_id"
+  add_index "categories", ["user_id"], name: "index_categories_on_user_id", using: :btree
 
   create_table "finders", force: :cascade do |t|
     t.string   "name"
@@ -32,8 +35,8 @@ ActiveRecord::Schema.define(version: 20160613183302) do
     t.text     "description", default: ""
   end
 
-  add_index "finders", ["category_id"], name: "index_finders_on_category_id"
-  add_index "finders", ["user_id"], name: "index_finders_on_user_id"
+  add_index "finders", ["category_id"], name: "index_finders_on_category_id", using: :btree
+  add_index "finders", ["user_id"], name: "index_finders_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",       null: false
@@ -51,7 +54,10 @@ ActiveRecord::Schema.define(version: 20160613183302) do
     t.string   "role",                   default: "author"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "finders", "categories"
+  add_foreign_key "finders", "users"
 end
